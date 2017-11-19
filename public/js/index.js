71116bbfb9a3ebb9e010fb8cecebe8156ccb6b3c
dreaddy <dreaddymck@gmail.com>
2017-11-19 15:37:14 +0000

    foo n stuff

--- public/js/index.js
+++ public/js/index.js
@@ -5,12 +5,18 @@ $( document ).ready( function () {
 		e.preventDefault();
 
 		$('.btn-toggle').prop('disabled', true);
+		$('.btn-toggle').val("Wait");
 
 		$.post('/serial/toggle',{}).done(function(response){
 
 			console.log(response)
 
-			$('.btn-toggle').prop('disabled', false);
+			setTimeout(	function(){
+
+				$('.btn-toggle').prop('disabled', false);
+				$('.btn-toggle').val("Toggle");
+
+			},3000);
 
 		})
 	})
--- routes/serial.js
+++ routes/serial.js
@@ -5,8 +5,11 @@ const serial 	= require('../lib/serial');
 module.exports = function(app) {
 
 	app.all('/serial/toggle', (req, res) => {
-		serial.init();
-		res.send({response:true})
+		serial.init().then((response)=>{
+			res.send({response:response})
+		}).catch((e)=>{
+			res.send({response:e})
+		});
 	})
 
 }
