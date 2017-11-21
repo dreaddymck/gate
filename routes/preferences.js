531c0305b1c3b9ac77a8eb81e4c201108d4095c1
dreaddy <dreaddymck@gmail.com>
2017-11-21 11:31:57 -0500

    return null in routes

--- routes/preferences.js
+++ routes/preferences.js
@@ -7,6 +7,7 @@ module.exports = function(app) {
 	app.get ([ '/preferences/theme'], (req, res) => {
 
 		res.send( preferences.theme_set(req,res) );
+		return null;
 
 	})
 
--- routes/serial.js
+++ routes/serial.js
@@ -7,8 +7,10 @@ module.exports = function(app) {
 	app.all('/serial/toggle', (req, res) => {
 		serial.init().then((response)=>{
 			res.send(response)
+			return null;
 		}).catch((e)=>{
 			res.send(e)
+			return null;
 		});
 	})
 
