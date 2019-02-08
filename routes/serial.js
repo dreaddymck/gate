7627090a4060055619ebc92a650754bf677c1ec5
dreaddy <dreaddymck@gmail.com>
2019-02-08 09:51:40 -0500

    mass update...sort of

--- routes/serial.js
+++ routes/serial.js
@@ -5,11 +5,13 @@ const serial 	= require('../lib/serial');
 module.exports = function(app) {
 
 	app.all('/serial/toggle', (req, res) => {
-		serial.init(app).then((response)=>{
-			res.send(response)
+		serial.toggle(app).then((response)=>{
+			console.log(response);
+			res.send(response);
 			return null;
 		}).catch((e)=>{
-			res.send(e)
+			console.log(e);
+			res.send(e);
 			return null;
 		});
 	})
