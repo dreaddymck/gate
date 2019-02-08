5c53c37057bda39d94de681bcc2716a87a95ad31
dreaddy <dreaddymck@gmail.com>
2019-02-08 12:33:35 -0500

    formatting

--- public/js/index.js
+++ public/js/index.js
@@ -1,46 +1,48 @@
-$( document ).ready( function () {
+$(document).ready(function () {
 
-	$('form[name=gate]').submit(function(e){
+	$('form[name=gate]').submit(function (e) {
 
-		e.preventDefault();
+			e.preventDefault();
 
-		$('.btn-toggle').prop('disabled', false);
-		$('.btn-toggle').removeClass('btn-primary');
-		$('.btn-toggle').addClass('btn-warning');
-		$('.btn-toggle').val("Please wait");
+			$('.btn-toggle').prop('disabled', false);
+			$('.btn-toggle').removeClass('btn-primary');
+			$('.btn-toggle').addClass('btn-warning');
+			$('.btn-toggle').val("Please wait");
 
-		$.post('/serial/toggle',{}).done(function(response){
+			$.post('/serial/toggle', {}).done(function (response) {
 
-			//console.log(response.status)
+				//console.log(response.status)
 
-			if( response.status ){
-				message.display(response.status, "message_danger");
-			}
+				if (response.status) {
+					message.display(response.status, "message_danger");
+				}
 
-			setTimeout(	function(){
+				setTimeout(function () {
 
-				$('.btn-toggle').prop('disabled', false);
-				$('.btn-toggle').removeClass('btn-warning');
-				$('.btn-toggle').addClass('btn-primary');
-				$('.btn-toggle').val("Toggle open/close");
+					$('.btn-toggle').prop('disabled', false);
+					$('.btn-toggle').removeClass('btn-warning');
+					$('.btn-toggle').addClass('btn-primary');
+					$('.btn-toggle').val("Toggle Open/Close");
 
-			},3000);
+				}, 3000);
 
-		})
-	}),
-    $(".theme-selection").on('click', 'li a', function(e){
+			})
+		}),
+		$(".theme-selection").on('click', 'li a', function (e) {
 
-    	e.preventDefault();
+			e.preventDefault();
 
-    	$.get('/preferences/theme', {theme: $(this).attr("value")} ).done(function(response){
-    		location.reload()
-		})
+			$.get('/preferences/theme', {
+				theme: $(this).attr("value")
+			}).done(function (response) {
+				location.reload()
+			})
 
-    });
+		});
 
 	$('.btn-toggle').prop('disabled', false);
 	$('.btn-toggle').removeClass('btn-warning');
 	$('.btn-toggle').addClass('btn-primary');
-	$('.btn-toggle').val("Toggle open/close");
+	$('.btn-toggle').val("Toggle Open/Close");
 
 });
