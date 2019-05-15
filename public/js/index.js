bf182dd9c62c0bef5b2b4761d34240c12a5878db
dreaddy <dreaddymck@gmail.com>
2019-05-15 15:28:29 -0500

    attempt to speed up load. default image is base64 anime video will show when loaded

--- public/js/index.js
+++ public/js/index.js
@@ -2,47 +2,55 @@ $(document).ready(function () {
 
 	$('form[name=gate]').submit(function (e) {
 
-			e.preventDefault();
+		e.preventDefault();
 
-			$('.btn-toggle').prop('disabled', false);
-			$('.btn-toggle').removeClass('btn-primary');
-			$('.btn-toggle').addClass('btn-warning');
-			$('.btn-toggle').val("Please wait");
+		$('.btn-toggle').prop('disabled', false);
+		$('.btn-toggle').removeClass('btn-primary');
+		$('.btn-toggle').addClass('btn-warning');
+		$('.btn-toggle').val("Please wait");
 
-			$.post('/serial/toggle', {}).done(function (response) {
+		$.post('/serial/toggle', {}).done(function (response) {
 
-				//console.log(response.status)
+			//console.log(response.status)
 
-				if (response.status) {
-					message.display(response.status, "message_danger");
-				}
+			if (response.status) {
+				message.display(response.status, "message_danger");
+			}
 
-				setTimeout(function () {
+			setTimeout(function () {
 
-					$('.btn-toggle').prop('disabled', false);
-					$('.btn-toggle').removeClass('btn-warning');
-					$('.btn-toggle').addClass('btn-primary');
-					$('.btn-toggle').val("Toggle Open/Close");
+				$('.btn-toggle').prop('disabled', false);
+				$('.btn-toggle').removeClass('btn-warning');
+				$('.btn-toggle').addClass('btn-primary');
+				$('.btn-toggle').val("Toggle Open/Close");
 
-				}, 3000);
+			}, 3000);
 
-			})
-		}),
-		$(".theme-selection").on('click', 'li a', function (e) {
+		})
+	}),
+	$(".theme-selection").on('click', 'li a', function (e) {
 
-			e.preventDefault();
+		e.preventDefault();
 
-			$.get('/preferences/theme', {
-				theme: $(this).attr("value")
-			}).done(function (response) {
-				location.reload()
-			})
+		$.get('/preferences/theme', {
+			theme: $(this).attr("value")
+		}).done(function (response) {
+			location.reload()
+		})
 
-		});
+	});
 
 	$('.btn-toggle').prop('disabled', false);
 	$('.btn-toggle').removeClass('btn-warning');
 	$('.btn-toggle').addClass('btn-primary');
 	$('.btn-toggle').val("Toggle Open/Close");
-
+	if(typeof globals !== 'undefined'){
+		if(globals.image){
+			let img=new Image();
+			img.src = globals.image
+			img.onload = function(){
+				$('#image').attr({"src": img.src, "width": "100%", "height": "100%" })
+			}			
+		}
+	}
 });
